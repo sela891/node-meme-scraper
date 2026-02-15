1c52177caf3e9cc7e55785aa2d33b02525f7f736
sela891 <laetitia.sengseis@gmail.com>
2026-02-15 23:32:29 +0100

    Implementing non-optional and optinal changes

--- index.js
+++ index.js
@@ -1,47 +1,41 @@
 // Node file system and buffer needed to save images.
 import { Buffer } from 'node:buffer';
 import fs from 'node:fs';
-// Document query selector not allowed by eslint, therefore using cheerio to select images
 import * as cheerio from 'cheerio';
 
-async function getImageLinks() {
-  const linkMemeGen = 'https://memegen-link-examples-upleveled.netlify.app/';
+const folderMemes = 'memes';
+// Check if folderMemes exists; if not, create it
+if (!fs.existsSync(folderMemes)) {
+  fs.mkdirSync(folderMemes, { recursive: true }); // if the recursive option is set to true,it allows the creation of nested directories
+  console.log(`Folder '${folderMemes}' created.`);
+} else {
+  console.log(`Folder'${folderMemes}' already exists.`);
+}
+// Fetching the URLs from memegen and saving it as text; extracting the img URLs from the HTML Data and saving 10 URLs in an array
+
+const linkMemeGen = 'https://memegen-link-examples-upleveled.netlify.app';
+
+try {
   const response = await fetch(linkMemeGen);
   const htmlData = await response.text();
   const $ = cheerio.load(htmlData);
-  const imgObjects = $('img');
-  const imgLinks = [];
-  for (let i = 0; i < 10; i++) {
-    imgLinks.push($(imgObjects[i]).attr('src'));
-  }
-  return imgLinks;
-}
+  const imgUrls = $('img').slice(0, 10).get();
 
-async function saveImages() {
-  const tenImgLinks = await getImageLinks();
-  for (let i = 1; i <= 10; i++) {
-    const response = await fetch(tenImgLinks[i - 1]);
-    const arrayBuffer = await response.arrayBuffer();
+  for (let i = 0; i < imgUrls.length; i++) {
+    const tenImgUrls = $(imgUrls[i]).attr('src');
+    const imgResponse = await fetch(tenImgUrls);
+    const arrayBuffer = await imgResponse.arrayBuffer();
     const buffer = Buffer.from(arrayBuffer);
     let fileNumber = '';
-    if (i < 10) {
-      fileNumber = '0' + i;
+    if (i + 1 < 10) {
+      fileNumber = '0' + (i + 1);
     } else {
       fileNumber = '10';
     }
-    const folderMemes = 'memes';
-    // Check if folderMemes exists; if not, create it
-    if (!fs.existsSync(folderMemes)) {
-      fs.mkdirSync(folderMemes, { recursive: true });
-    }
     fs.writeFileSync(`${folderMemes}/${fileNumber}.jpg`, buffer);
   }
-}
 
-saveImages()
-  .then(() => {
-    console.log('Successfully saved 10 memes!');
-  })
-  .catch((err) => {
-    console.error('An error occurred:', err);
-  });
+  console.log('Successfully saved 10 memes!');
+} catch (err) {
+  console.error('An error occurred:', err);
+}
